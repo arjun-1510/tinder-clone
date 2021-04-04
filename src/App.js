d2aff7f3b28ab73a1b18fcb002c356da5ca9519c
arjun-1510 <Arjung1510@gmail.com>
2021-04-04 19:30:13 +0530

    icons installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>lets builts</h1>
-
-      {/* Header */ }
+    <div className="app">
+      <h1>lets builts the merg tinder</h1>
+      <Header />
+      
       {/* TinderCards /*}
       {/* SwipeButton */}
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
 
 function Header() {
     return (
-        <div>
+        <div className="header">
+            <h2>I am header</h2>
+            <PersonIcon />
             
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
